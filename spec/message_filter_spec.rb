3b190416ed5acdbfb6373a7d3b2d56c385503cc4
MaiInoue <6viva.la.vida9@gmail.com>
2012-06-05 15:31:02 +0900

    refactoring test

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,4 +1,15 @@
 require 'message_filter'
 
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
+
+  it 'should detect message with NG word' do
+    @filter.detect?('hello from foo').should == true
+  end
+  
+  it 'should not detect message without NG word' do
+    @filter.detect?('hello, world!').should == false
+  end
 end
